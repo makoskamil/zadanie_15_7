69c17200d36df1e9e812911b364ad7a93f668d2d
Kamil Makoś <makos.kamil@o2.pl>
2018-06-21 08:52:02 +0200

    timer

---
+++ script.js
@@ -0,0 +1,90 @@
+class Stopwatch extends React.Component {
+    constructor() {
+        super();
+        this.running = false;        
+        
+        this.state = {
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            }
+        };
+    }
+    
+    reset() {
+        this.setState({
+            times: {
+                minutes: 0,
+                seconds: 0,
+                miliseconds: 0
+            }
+        });
+    }
+    
+    format(times) {
+        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+    }
+
+
+    start() {
+        if (!this.running) {
+            this.running = true;
+            this.watch = setInterval(() => this.step(), 10);
+        }
+    }
+    
+    step() {
+        if (!this.running) return;
+        this.calculate();
+    }
+    
+    calculate() {
+        const times = Object.assign({}, this.state.times);
+
+        times.miliseconds += 1;
+        if (times.miliseconds >= 100) {
+            times.seconds += 1;
+            times.miliseconds = 0;
+        }
+        if (times.seconds >= 60) {
+            times.minutes += 1;
+            times.seconds = 0;
+        }
+
+        this.setState({times});
+    }
+    
+    stop() {
+        this.running = false;
+        clearInterval(this.watch);
+    }
+
+    render() {
+        return (
+            <div>
+                <nav class="controls">
+                    <a href="#" class="button" onClick={this.start.bind(this)}>Start</a>
+                    <a href="#" class="button" onClick={this.stop.bind(this)}>Stop</a>
+                </nav>
+                <div class="stopwatch">
+                    {this.format(this.state.times)}
+                </div>
+                <ul class="results"></ul>
+            </div>
+        );
+    }
+}
+
+function pad0(value) {
+    let result = value.toString();
+    if (result.length < 2) {
+        result = 0 + result;
+    }
+    return result;
+}
+
+ReactDOM.render(
+    <Stopwatch />,
+    document.getElementById('app')
+);
